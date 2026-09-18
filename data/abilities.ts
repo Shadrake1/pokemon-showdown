730324c75d074b4582e33e8986fd845c740461ba
Shadrake1 <130673769+Shadrake1@users.noreply.github.com>
2026-09-18 16:37:14 -0400

    Add custom Pokemon and moves

--- data/abilities.ts
+++ data/abilities.ts
@@ -5703,4 +5703,68 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
 		rating: 3,
 		num: -3,
 	},
+	atlanteanrage: {
+		//Gura transforms into Gura-Primal upon claiming a KO
+				shortDesc: "WIP, coming soon!",
+		flags: {},
+		name: "Atlantean Rage",
+		rating: 0,
+		num: 99,
+	},
+	mothernature: {
+		//Grass types recieve doubled defense in the sun
+			shortDesc: "WIP, coming soon!",
+		flags: {},
+		name: "Mother Nature",
+		rating: 0,
+		num: 98,
+	},
+	chaosincarnation: {
+		//Gains a random ability (minus certain ones like Wonder Guard) on switch-in
+			shortDesc: "WIP, coming soon!",
+		flags: {},
+		name: "Chaos Incarnation",
+		rating: 0,
+		num: 97,
+	},
+	gravitationalforce: {
+		//summons Gravity on switch-in
+			shortDesc: "WIP, coming soon!",
+		flags: {},
+		name: "Gravitational Force",
+		rating: 0,
+		num: 96,
+	},
+	temporalforce: {
+		//summons trickroom on switch-in
+			shortDesc: "WIP, coming soon!",
+		flags: {},
+		name: "Temporal Force",
+		rating: 0,
+		num: 95,
+	},
+	repopulate: {
+		//innate leftovers effect
+			shortDesc: "WIP, coming soon!",
+		flags: {},
+		name: "Repopulate",
+		rating: 0,
+		num: 94,
+	},
+	soulreaper: {
+		// physical moves have an x% chance to instakill (500x damage)
+			shortDesc: "WIP, coming soon!",
+		flags: {},
+		name: "Soul Reaper",
+		rating: 0,
+		num: 93,
+	},
+	twinsync: {
+		// //Twin sync multiplies their damage by 0.75x, but all attacks hit twice
+			shortDesc: "WIP, coming soon!",
+		flags: {},
+		name: "Twin Sync",
+		rating: 0,
+		num: 92,
+	},
 };
