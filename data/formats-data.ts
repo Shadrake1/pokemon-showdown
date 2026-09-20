823bdcde33a1871de6d8884000a89fa6d5b5b0f5
Shadrake1 <130673769+Shadrake1@users.noreply.github.com>
2026-09-20 13:44:06 -0400

    removed isnonstandard

--- data/formats-data.ts
+++ data/formats-data.ts
@@ -6255,177 +6255,134 @@ export const FormatsData: import('../sim/dex-species').SpeciesFormatsDataTable =
 		tier: "Illegal",
 	},
 	neurosama: {
-		isNonstandard: "Custom",
 		tier: "Vtuber",
 	},
 	evilneuro: {
-		isNonstandard: "Custom",
 		tier: "Vtuber",
 	},
 	filian: {
-		isNonstandard: "Custom",
 		tier: "Vtuber",
 	},
 	camila: {
-		isNonstandard: "Custom",
 		tier: "Vtuber",
 	},
 	ellieminibot: {
-		isNonstandard: "Custom",
 		tier: "Vtuber",
 	},
 	cerber: {
-		isNonstandard: "Custom",
 		tier: "Vtuber",
 	},
 	fallenshadow: {
-		isNonstandard: "Custom",
 		tier: "Vtuber",
 	},
 	airys: {
-		isNonstandard: "Custom",
 		tier: "Vtuber",
 	},
 	annytf: {
-		isNonstandard: "Custom",
 		tier: "Vtuber",
 	},
 	shylily: {
-		isNonstandard: "Custom",
 		tier: "Vtuber",
 	},
 	gawrgura: {
-		isNonstandard: "Custom",
 		tier: "Vtuber",
 	},
 	guraprimal: {
-		isNonstandard: "Custom",
 		tier: "Vtuber",
 	},
 	ceresfauna: {
-		isNonstandard: "Custom",
 		tier: "Vtuber",
 	},
 	hakosbaelz: {
-		isNonstandard: "Custom",
 		tier: "Vtuber",
 	},
 	chiurunika: {
-		isNonstandard: "Custom",
 		tier: "Vtuber",
 	},
 	moricaliope: {
-		isNonstandard: "Custom",
 		tier: "Vtuber",
 	},
 	laynalazar: {
-		isNonstandard: "Custom",
 		tier: "Vtuber",
 	},
 	haachama: {
-		isNonstandard: "Custom",
 		tier: "Vtuber",
 	},
 	akaihaato: {
-		isNonstandard: "Custom",
 		tier: "Vtuber",
 	},
 	inugamikorone: {
-		isNonstandard: "Custom",
 		tier: "Vtuber",
 	},
 	ninomaeinanis: {
-		isNonstandard: "Custom",
 		tier: "Vtuber",
 	},
 	takanashikiara: {
-		isNonstandard: "Custom",
 		tier: "Vtuber",
 	},
 	minikomew: {
-		isNonstandard: "Custom",
 		tier: "Vtuber",
 	},
 	toma: {
-		isNonstandard: "Custom",
 		tier: "Vtuber",
 	},
 	tsukomosana: {
-		isNonstandard: "Custom",
 		tier: "Vtuber",
 	},
 	ourokronii: {
-		isNonstandard: "Custom",
 		tier: "Vtuber",
 	},
 	nanashimumei: {
-		isNonstandard: "Custom",
 		tier: "Vtuber",
 	},
 	usadapekora: {
-		isNonstandard: "Custom",
 		tier: "Vtuber",
 	},
 	fuwamoco: {
-		isNonstandard: "Custom",
 		tier: "Vtuber",
 	},
 	dokibird: {
-		isNonstandard: "Custom",
 		tier: "Vtuber",
 	},
 	chibidoki: {
-		isNonstandard: "Custom",
 		tier: "Vtuber",
 	},
 	meowzarts: {
-		isNonstandard: "Custom",
 		tier: "Vtuber",
 	},
 	kyarashard: {
-		isNonstandard: "Custom",
 		tier: "Vtuber",
 	},
 	kosekibijou: {
-		isNonstandard: "Custom",
 		tier: "Vtuber",
 	},
 	megalodonvt: {
-		isNonstandard: "Custom",
 		tier: "Vtuber",
 	},
 	gigimurin: {
-		isNonstandard: "Custom",
 		tier: "Vtuber",
 	},
 	minatoaqua: {
-		isNonstandard: "Custom",
 		tier: "Vtuber",
 	},
 	zentreya: {
-		isNonstandard: "Custom",
 		tier: "Vtuber",
 	},
 	vedal: {
-		isNonstandard: "Custom",
 		tier: "Vtuber",
 	},
 	neuropede: {
-		isNonstandard: "Custom",
 		tier: "Vtuber",
 	},
 	bao: {
-		isNonstandard: "Custom",
 		tier: "Vtuber",
 	},
 
 //not vtubers
 	sixln: {
-		isNonstandard: "Custom",
 		tier: "Other",
 	},
 	shoppingcart: {
-		isNonstandard: "Custom",
 		tier: "Other",
 	},
 };
